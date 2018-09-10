02ebed6638fbf29c0d7c1c8d7da6aed52992f75d
Unknown <unknown@example.com>
2018-09-10 18:54:50 +0700

    chang signt

--- Adder.java
+++ Adder.java
@@ -5,7 +5,7 @@ class Adder {
         System.out.print("Input Numer 1 = ");int a = sc.nextInt();
         System.out.print("Input Numer 2 = ");int b = sc.nextInt();
 
-        int sum = a+b;
+        int sum = a-b;
         
         System.out.println("Result Combination = "+sum);
 
